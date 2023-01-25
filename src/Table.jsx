0a7cfbe711406a60b1a1c0c6fb8f7f9ce7f19ced
Sadanand Gopal <sadanandgpl@gmail.com>
2023-01-25 19:31:49 +0530

    changes

--- src/Table.jsx
+++ src/Table.jsx
@@ -4,9 +4,9 @@ let arr = [
     b: 2,
     c: 3,
     child: [
-      { d: 4, e: 5, f: 6 },
-      { d: 14, e: 15, f: 16 },
-      { d: 114, e: 115, f: 116 },
+      { e: 5, f: 6 },
+      { e: 15, f: 16 },
+      { e: 115, f: 116 },
     ],
   },
   {
@@ -14,11 +14,11 @@ let arr = [
     b: 20,
     c: 30,
     child: [
-      { d: 40, e: 50, f: 60 },
-      { d: 140, e: 150, f: 160 },
+      { e: 50, f: 60 },
+      { e: 150, f: 160 },
     ],
   },
-  { a: 100, b: 200, c: 300, child: [{ d: 400, e: 500, f: 600 }] },
+  { a: 100, b: 200, c: 300, child: [{ e: 500, f: 600 }] },
 ];
 import React from "react";
 export default function Table() {
@@ -29,7 +29,7 @@ export default function Table() {
           <th>Company</th>
           <th>Product</th>
           <th>Country</th>
-          <th>Pin-code</th>
+          {/* <th>Pin-code</th> */}
           <th>Status</th>
           <th>Amount</th>
         </tr>
@@ -45,12 +45,11 @@ export default function Table() {
               </tr>
               {el.child.map((childEl, ii) => (
                 <tr key={ii + i}>
-                  <td>{childEl.d}</td>
+                  {/* <td>{childEl.d}</td> */}
                   <td>{childEl.e}</td>
                   <td>{childEl.f}</td>
                 </tr>
               ))}
-              
             </React.Fragment>
           );
         })}
